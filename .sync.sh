c7cc42d35bd59dadf4e4a8e031ae0af4cdb484d2
AsexyLordRevan <155589214+AsexyLordRevan@users.noreply.github.com>
2024-11-09 21:11:41 -0800

    It works now !

--- .sync.sh
+++ .sync.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-
-cp -f .config/colours/colours.conf .config/hypr/colours.conf
-cp -f .config/colours/colours.conf .confing/waybar/colours.css
-cp -f .config/colours/colours.conf .config/rofi/colours.rosi
-cp -f .config/colours/colours.conf .config/betterdiscord
-cd ~/.mozilla/firefox
-if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
-then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
-else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
-fi
-cp -f ~/.config/colours/colours.css .mozilla/firefox/$PROFPATH/chrome/colours.css
+awk 'NR>=1 && NR<=79' ~/.config/colours/colours.conf > .config/hypr/colours.conf
+awk 'NR>=80 && NR<=107' ~/.config/colours/colours.conf > .config/waybar/colours.css
+awk 'NR>=109 && NR<=140' ~/.config/colours/colours.conf > .config/rofi/colours.rasi
+awk 'NR>=80 && NR<=107' ~/.config/colours/colours.conf > .config/betterdiscord/themes/colours.css
 echo "Colours synced"
-
-killall waybar &
+killall waybar
 waybar &
 hyprctl reload &
 
 echo "Software reloaded"
+
+
+
+
+
