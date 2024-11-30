a458de5e8026d4b0531030d4e06dde6f5fbc898a
AsexyLordRevan <155589214+AsexyLordRevan@users.noreply.github.com>
2024-11-30 10:22:29 -0800

    Update .sync.sh

--- .sync.sh
+++ .sync.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-awk 'NR>=1 && NR<=79' ~/.config/colours/colours.conf > .config/hypr/colours.conf
-awk 'NR>=80 && NR<=107' ~/.config/colours/colours.conf > .config/waybar/colours.css
-awk 'NR>=109 && NR<=140' ~/.config/colours/colours.conf > .config/rofi/colours.rasi
-awk 'NR>=80 && NR<=107' ~/.config/colours/colours.conf > .config/betterdiscord/themes/colours.css
+
+awk 'NR>=1 && NR<=17' ~/.config/colours/colours.conf > .config/hypr/colours.conf
+awk 'NR>=21 && NR<=35' ~/.config/colours/colours.conf > .config/waybar/colours.css
+awk 'NR>=38 && NR<=54' ~/.config/colours/colours.conf > .config/rofi/colours.rasi
+awk 'NR>=21 && NR<=35' ~/.config/colours/colours.conf > .config/BetterDiscord/themes/colours.css
 echo "Colours synced"
+
 killall waybar
 waybar &
-hyprctl reload &
+systemctl --user stop mako && mako -c ~/.config/mako/config &
 
 echo "Software reloaded"
-
-
-
-
-
