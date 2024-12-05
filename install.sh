13b497547443a8eb689cf3713c22d34d30834133
AsexyLordRevan <155589214+AsexyLordRevan@users.noreply.github.com>
2024-12-05 14:28:15 -0800

    Bugfix
    Fixed bug where files would be read only if run as sudo, fixed file paths

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 set -e
 cd ~
+if [$USER=root]; then
+  echo "DO NOT RUN AS ROOT"
+  exit
+fi
 #-----Installs/Updates all required packages + updates system-----#
 sudo pacman -S discord, firefox, kitty, rofi, hypr{land,lock,idle,paper}
 sudo pacman -Syu
@@ -9,43 +13,37 @@ echo "Packages installed, system updated"
 git clone https://github.com/asexylordrevan/asexy_dots ~/asexy_dots
 echo "Repository cloned"
 #-----Hyprland-----#
-if [-d ~/.config/hypr]; then
-  mv ~/.config/hypr ~/.config/hypr.backup
-fi mv ~/asexy_dots/hypr ~/.config/hypr
+if [-d ~/.config/hypr]; then mv ~/.config/hypr ~/.config/hypr.backup
+fi 
+mv ~/asexy_dots/hypr ~/.config/hypr
 echo "Hyprland config installed"
 #-----Waybar-----#
-if [-d ~/.config/waybar]; then
-  mv ~/.config/waybar ~/.config/waybar.backup
+if [-d ~/.config/waybar]; then mv ~/.config/waybar ~/.config/waybar.backup
 fi
 mv ~/asexy_dots/waybar ~/.config/waybar
 echo "Waybar config installed"
 #-----Kitty-----#
-if [-d ~/.config/kitty]; then
-  mv ~/.config/kitty ~/.config/kitty.backup
+if [-d ~/.config/kitty]; then mv ~/.config/kitty ~/.config/kitty.backup
 fi
 mv ~/asexy_dots/kitty ~/.config/kitty
 echo "Kitty config installed"
 #-----Rofi-----#
-if [-d ~/.config/rofi]; then
-  mv ~/.config/rofi ~/.config/rofi.backup
+if [-d ~/.config/rofi]; then mv ~/.config/rofi ~/.config/rofi.backup
 fi
 mv ~/asexy_dots/rofi ~/.config/rofi
 echo "Rofi config installed"
 #-----Mako-----#
-if [-d ~/.config/mako]; then
-  mv ~/.config/mako ~/.config/mako.backup
+if [-d ~/.config/mako]; then mv ~/.config/mako ~/.config/mako.backup
 fi
 mv ~/asexy_dots/mako ~/.config/mako
 echo "Mako config installed"
 #-----Wallpapers-----#
-if [-d ~/Documents/wallpapers]; then
-  mv ~/Documents/wallpapers ~/Documents/wallpapers.backup
+if [-d ~/Documents/wallpapers]; then mv ~/Documents/wallpapers ~/Documents/wallpapers.backup
 fi
 mv ~/asexy_dots/wallpapers ~/Documents/wallpapers
 echo "Wallpapers installed"
 #-----Sync-----#
-if [-f ~/.config]; then
-  mv ~/asexy_dots/.sync.sh .sync.sh
+if [-f ~/.config]; then mv ~/asexy_dots/.sync.sh .sync.sh
 #-----Install BetterDiscord-----#
 git clone https://github.com/BetterDiscord/BetterDiscord.git ~/BetterDiscord
 cd BetterDiscord
@@ -56,8 +54,7 @@ pnpm inject
 echo "BetterDiscord installed"
 mv ~/asexy_dots/BetterDiscord/catppuccin.theme.css ~/.config/BetterDicord/themes/asexy_catppuccin.theme.css
 #-----Colours-----#
-if [-d ~/.config/colours]; then
-  mv ~/.config/colours ~/.config/colours_backup
+if [-d ~/.config/colours]; then mv ~/.config/colours ~/.config/colours_backup
 fi
 mv ~/asexy_dots/colours ~/.config/colours
 echo "Colours installed"
